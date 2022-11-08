b55a286544a33907e246ac8905fc196e7f71e16c
Audrey Jiang <ajiang@lcs.on.ca>
2022-11-08 14:18:38 -0500

    Initial Commit

---
+++ NameBadgeSolution/NameBadgeSolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameBadgeSolutionApp.swift
+//  NameBadgeSolution
+//
+//  Created by Audrey Jiang on 2022-11-08.
+//
+
+import SwiftUI
+
+@main
+struct NameBadgeSolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
